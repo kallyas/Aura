b37b84c6cee0bcfbb59a1ff88bf3c418ddd28bd9
Rose <rosemichellw@gmail.com>
2026-02-17 13:01:02 +0300

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Rose Visuals on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
